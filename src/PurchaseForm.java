9e1c1a048e8ada6507595555d19c348bf00d3565
karinems <karine.santos11@fatec.sp.gov.br>
2020-11-09 19:05:23 -0300

    Criando tela de compra e registro

---
+++ src/PurchaseForm.java
@@ -0,0 +1,70 @@
+
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class PurchaseForm extends Application{
+
+	private Label labelItem = new Label("Item");
+	private Label labelQuantidade = new Label("Quantidade");
+	private Label labelTotal = new Label("Total");
+    private Label labelFormadePagamento = new Label("Forma de Pagamento");
+    private Label labelModeloRoupa = new Label("Camisa Polo");
+    private Label labelValor = new Label("R$ 59,90");
+    
+    private ComboBox<String> comboBoxQuantidade = new ComboBox<>();
+    private ComboBox<String> comboBoxFormadePagamento = new ComboBox<>();
+    
+    private Button buttonComprar = new Button("Comprar");
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Pane pane = new Pane();
+        Scene scene = new Scene(pane, 600, 420);
+        
+        stage.setScene(scene);
+        stage.setTitle("Compra");
+        stage.show();
+
+        pane.getChildren().addAll(labelItem, labelQuantidade, labelTotal, labelFormadePagamento,
+        		labelModeloRoupa, labelValor, comboBoxQuantidade, comboBoxFormadePagamento, buttonComprar);
+        
+        //Item
+        labelItem.relocate(20,20);
+        labelModeloRoupa.setStyle("-fx-font-size:40");
+        labelModeloRoupa.relocate(20, 30); 
+        
+        //Quantidade
+        labelQuantidade.relocate(320,50);
+       	comboBoxQuantidade.relocate(470, 50);
+       	comboBoxQuantidade.setMinWidth(110);
+       	comboBoxQuantidade.setMaxWidth(110);
+       	comboBoxQuantidade.getItems().addAll("1", "2", "3");
+        
+        //Total
+        labelTotal.relocate(20,150);
+        labelValor.setStyle("-fx-font-size:40");
+        labelValor.relocate(20, 165); 
+               
+        //Pagamento
+        labelFormadePagamento.relocate(320, 150);
+        comboBoxFormadePagamento.relocate(470, 150);
+        comboBoxFormadePagamento.setMinWidth(110);
+        comboBoxFormadePagamento.setMaxWidth(110);
+        comboBoxFormadePagamento.getItems().addAll("Boleto", "Débito", "Crédito");
+                
+        buttonComprar.setMinSize(220, 15);
+        buttonComprar.setStyle("-fx-font-size:20");
+        buttonComprar.relocate(200, 320);
+
+    }
+
+    public static void main(String[] args) {
+        Application.launch(PurchaseForm.class, args);
+    }
+}
---
+++ src/RegisterForm.java
@@ -0,0 +1,106 @@
+
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class RegisterForm extends Application{
+
+	private Label labelNome = new Label("Nome");
+	private Label labelCpf = new Label("CPF");
+	private Label labelEmail = new Label("E-mail");
+    private Label labelSenha = new Label("Senha");
+    private Label labelTelefone = new Label("Telefone");
+    private Label labelCep = new Label("CEP");
+    private Label labelLogradouro = new Label("Logradouro");
+    private Label labelRua = new Label("Rua");
+    private Label labelReferencia = new Label("Ponto de referência");
+    
+    private TextField textFieldNome = new TextField();
+    private TextField textFieldCpf = new TextField();
+    private TextField textFieldEmail = new TextField();
+    private TextField textFieldSenha = new TextField();
+    private TextField textFieldTelefone = new TextField();
+    private TextField textFieldCep = new TextField();
+    private TextField textFieldLogradouro = new TextField();
+    private TextField textFieldRua = new TextField();
+    private TextField textFieldReferencia = new TextField();
+    
+    private Button buttonCadastrarUsuario = new Button("Cadastrar");
+    
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Pane pane = new Pane();
+        Scene scene = new Scene(pane, 600, 420);
+        
+        stage.setScene(scene);
+        stage.setTitle("Cadastro");
+        stage.show();
+
+        pane.getChildren().addAll(labelNome, labelCpf, labelEmail, labelSenha,
+        		labelTelefone, labelCep, labelLogradouro, labelRua, labelReferencia,
+                textFieldNome, textFieldCpf, textFieldEmail, textFieldSenha,
+                textFieldTelefone, textFieldCep, textFieldLogradouro, textFieldRua, textFieldReferencia,
+                buttonCadastrarUsuario);
+        
+        //1 Nome
+        labelNome.relocate(10,20);
+        textFieldNome.setMinSize(202, 24);
+        textFieldNome.relocate(100,20);
+        
+        //2 CPF
+        labelCpf.relocate(330,20);
+        textFieldCpf.setMinSize(202, 24);
+        textFieldCpf.relocate(385, 20);
+        
+        //3 Email
+        labelEmail.relocate(10,70);
+        textFieldEmail.setMinSize(202, 24);
+        textFieldEmail.relocate(100,70);
+        
+        //4 Senha
+        labelSenha.relocate(330, 70);
+        textFieldSenha.setMinSize(202, 24);
+        textFieldSenha.relocate(385, 70);
+
+        //5 Telefone
+        labelTelefone.relocate(10,120);
+        textFieldTelefone.setMinSize(202, 24);
+        textFieldTelefone.relocate(100,120);
+        
+        //6 CEP
+        labelCep.relocate(330,120);
+        textFieldCep.setMinSize(202, 24);
+        textFieldCep.relocate(385,120);
+
+        //7 Logradouro
+        labelLogradouro.relocate(10,170);
+        textFieldLogradouro.setMinSize(202, 24);
+        textFieldLogradouro.relocate(100,170);
+        
+        //8 Rua
+        labelRua.relocate(330,170);
+        textFieldRua.setMinSize(202, 24);
+        textFieldRua.relocate(385,170);
+        
+        //9 Ponto de referencia
+        labelReferencia.relocate(10,220);
+        textFieldReferencia.setMinSize(435, 24);
+        textFieldReferencia.relocate(153,220);
+        
+        buttonCadastrarUsuario.setMinSize(200, 103);
+        buttonCadastrarUsuario.setStyle("-fx-font-size:30");
+        buttonCadastrarUsuario.relocate(385, 280);
+
+    }
+
+    public static void main(String[] args) {
+        Application.launch(RegisterForm.class, args);
+    }
+}
+
